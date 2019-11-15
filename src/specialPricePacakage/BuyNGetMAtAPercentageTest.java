ddea52afaecf4b654b38bc1d3e769a4fa2f92ef8
someone <someone>
2019-11-15 14:31:23 -0500

    added some comments

---
+++ src/specialPricePacakage/BuyNGetMAtAPercentageTest.java
@@ -0,0 +1,7 @@
+package specialPricePacakage;
+
+import mainPackage.formatBigDecimal;
+
+public class BuyNGetMAtAPercentageTest extends formatBigDecimal {
+    // tested in checkoutOrderTest class
+}
---
+++ src/specialPricePacakage/NForXDollarTest.java
@@ -0,0 +1,34 @@
+package specialPricePacakage;
+
+import mainPackage.Inventory;
+import mainPackage.Item;
+import mainPackage.formatBigDecimal;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class NForXDollarTest extends formatBigDecimal {
+    private Inventory inventory;
+    private NForXDollar NFXD;
+
+    @Before
+    public void setup() {
+        inventory = new Inventory();
+        inventory.storeItems(new Item("Bread", false, getFormat(1.99), false));
+        inventory.storeItems(new Item("Grape", true, getFormat(0.99), false));
+
+    }
+
+    @Test
+    public void setItemSpecialOfferReturnsItemOrderTotalPrice(){
+        NFXD = new NForXDollar(3, getFormat(5.00), (Integer) null); //test in quantity
+        assertEquals(getFormat(10.00), getFormat(NFXD.calculatePrice(inventory.getItemHashMap().get("Bread"), 6)));
+
+        NFXD = new NForXDollar(3.00, getFormat(2.00), null); //test in weight
+        assertEquals(getFormat(3.98), getFormat(NFXD.calculatePrice(inventory.getItemHashMap().get("Grape"), 5.00)));
+
+    }
+}
